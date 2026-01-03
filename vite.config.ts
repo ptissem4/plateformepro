cae0ee4069041b4aa460d642edeff2253b78450e
ptissem4 <ecomwhalemedia@gmail.com>
2026-01-03 21:10:24 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // CRUCIAL: Permet au site de fonctionner même s'il n'est pas à la racine du domaine
+  base: '/', // Utilisation de chemin absolu pour Netlify (standard SPA)
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    sourcemap: false
+  },
+  server: {
+    host: true
   }
 });
